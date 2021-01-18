3c8bedfa9eb3bdd4a949075260acc1dd96e404ef
Alihewaigh <alihewaigh@gmail.com>
2021-01-18 13:55:20 +0800

    Initial Commit

---
+++ BLE Integration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLE Integration
+//
+//  Created by Ali Hewiagh on 18/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
